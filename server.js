50eabc94df8f199d2b17ab08341e1df0ebe789dd
Stephen Oney <swloney@gmail.com>
2012-02-24 21:58:39 -0500

    bleh

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express');
 
 var app = express.createServer();
@@ -7,3 +8,4 @@ var port = process.env.PORT || 8000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+*/
